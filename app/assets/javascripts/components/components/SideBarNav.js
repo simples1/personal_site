3908648debf3167eed009389f71d0ce79d21c179
simples1 <abdullaaden@outlook.com>
2017-11-29 01:20:02 +0000

    adds short intro

--- app/assets/javascripts/components/components/SideBarNav.js
+++ app/assets/javascripts/components/components/SideBarNav.js
@@ -11,8 +11,13 @@ class SideBarNav extends Component {
     return (
       <div className="side_bar_container">
         <div id="image_place" className="img-circle"></div>
+        <div className="intro">
+          <p>
+            Ruby on Rails Consultant. Helpful information on Rails development and strategy.
+          </p>
+        </div>
         <ul className="nav nav-pills nav-stacked">
-          <li role="presentation" class="active"><a href="#">About Me</a></li>
+          <li role="presentation" className="active"><a href="#">About Me</a></li>
           <li role="presentation"><a href="#">Portfolio</a></li>
           <li role="presentation"><a href="#">Blog</a></li>
         </ul>
